87dc31747ed113b50119dc96b301158e3cbaff35
Kittithong <koonkao.kr@gmail.com>
2026-07-27 00:23:23 +0700

    2513510111 Submit LabSheet04

---
+++ LabSheet04/src/TodoList.java
@@ -0,0 +1,36 @@
+
+public class TodoList {
+
+	public static void main(String[] args) {
+		DoublyLinkedList todoList = new DoublyLinkedList();
+
+		todoList.insert("Finish Homework");
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.insert("Laundry");
+		todoList.insert("Group Meeting");
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.insert(0, "Submit Report");
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.insert(2, "Buy Food");
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.insert("Go to gym");
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.remove();
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.remove(3);
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		todoList.remove(0);
+		System.out.println("To-Do List = " + todoList.traversal());
+
+		System.out.println("\nBackward Traversal " + todoList.backwardTraversal());
+
+	}
+
+}
